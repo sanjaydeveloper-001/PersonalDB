3d78b3d84bb2b6d4274e7308c3addd5de3aca9be
Sanjay <josephstudent001@gmail.com>
2026-03-28 18:40:58 +0530

    Added origin in code

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use(cookieParser());
 
 // CORS configuration
 app.use(cors({ 
-  origin: [process.env.CLIENT_URL1, process.env.CLIENT_URL2, 'http://localhost:3000'],
+  origin: [process.env.CLIENT_URL1, process.env.CLIENT_URL2, 'https://personaldb.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
