4e89b7cc30160520a6280a690cdeeb57cb650f5e
Sanjay <josephstudent001@gmail.com>
2026-04-20 14:36:23 +0530

    Handled passport to allow users

--- server/config/passport.js
+++ server/config/passport.js
@@ -24,7 +24,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/api/auth/google/callback", 
+      callbackURL: `${process.env.BASE_URL}/api/auth/google/callback`, 
       session: false,
     },
     async (accessToken, refreshToken, profile, done) => {
